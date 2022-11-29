70340f4a6e6b7c77d236409e9817d868ef23fb4b
CarlosPantin~ <carlospantin@live.com.mx>
2022-11-29 19:00:31 +0200

    Trap Triggers and Activation

--- My project/Assets/script/Activation.cs
+++ My project/Assets/script/Activation.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Activation : MonoBehaviour
 {
     public float damage;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,7 +30,7 @@ public class Activation : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            transform.GetComponent<Animator>().Play("Spikes");
+            anim.SetTrigger("activate");
         }
     }
 }
