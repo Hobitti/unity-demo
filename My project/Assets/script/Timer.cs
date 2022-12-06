9ed0919a4d76ceca62eb082d26492a0eaf514fbf
Stammer91 <73183215+Stammer91@users.noreply.github.com>
2022-12-06 10:04:33 +0200

    Update

---
+++ My project/Assets/script/Timer.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class Timer : MonoBehaviour
+{
+    public float timeValue = 181;
+    public TextMeshProUGUI timerText;
+
+    [SerializeField]
+    AudioClip sound;
+
+    //Update is called once per frame
+    void Update()
+    {
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime;
+        }
+        else
+        {
+            timeValue = 0;
+        }
+
+        DisplayTime(timeValue);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if (timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+
+        if (timeValue < 60)
+        {
+
+            timerText.color = Color.red;
+        }
+
+        if (timeValue == 60)
+        {
+            SoundManager.instance.PlaySingle(sound);
+        }
+        if (timeValue == 30)
+        {
+            SoundManager.instance.PlaySingle(sound);
+        }
+        if (timeValue == 10)
+        {
+            SoundManager.instance.PlaySingle(sound);
+        }
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+    }
+}
